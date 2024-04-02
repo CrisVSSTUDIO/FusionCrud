02c5f93eeed50ff4ddf8f80c707814537103fe24
Cristas <crista99r@gmail.com>
2024-04-02 11:57:59 +0100

    must verify email

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Auth\MustVerifyEmail;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable /* implements MustVerifyEmail */
+class User extends Authenticatable  implements MustVerifyEmail 
 {
     use HasApiTokens, HasFactory, Notifiable,HasRoles;
 
